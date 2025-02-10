084284902026bc6979badabf8bb34dcbde499699
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-02-10 13:26:49 -0500

    Fix operator in middle bug

--- Exercise 2/rpn.py
+++ Exercise 2/rpn.py
@@ -3,38 +3,47 @@ import sys
 #TODO: Make main method
 #TODO: Read from file
 #TODO: Print steps
-#BUG: The code cannot parse postfix expressions with operators in the middle
-    #of the expression. Example: 5 7 + 6 7 + * should evaluate to 156, code
-    #returns 100
 
 # Replace this comment with your implementations of the evaluate() and main()
 # functions.
 def evaluate(postfix_expression):
-    postfix_expression = postfix_expression.strip().split()
-    possibleoperators = ['+','*','/','-']
-    operators = []
     operands = []
+    possible_operators = ['+','*','/','-']
+    postfix_expression = postfix_expression.strip().split()
+    
+    #Loop through each character in the given postfix expression
     for element in postfix_expression:
-        if element in possibleoperators:
-            operators.append(element)
-        else:
+        #Add number elements to stack
+        if not element in possible_operators:
             operands.append(float(element))
-
-    i = 0
-    while len(operands) != 1:
-        operand2 = operands.pop()
-        operand1 = operands.pop()
-        match(operators[i]):
-            case('+'):
-                operands.append(operand1 + operand2)
-            case('*'):
-                operands.append(operand1 * operand2)
-            case('-'):
-                operands.append(operand1 - operand2)
-            case('/'):
-                operands.append(operand1 / operand2)
-        i += 1         
-    return(operands[0])
+        #Do postfix calculation with stack when operator found
+        else:
+            #Get operands
+            try:
+                operand2 = operands.pop()
+                operand1 = operands.pop()
+            #Handle invalid postfix expressions being passed
+            except:
+                print("There was an issue, please check your postfix expression.")
+                return None
+            #Add result of postfix calculation to stack
+            match(element):
+                case("+"):
+                    operands.append(operand1 + operand2)
+                case("*"):
+                    operands.append(operand1 * operand2)
+                case("-"):
+                    operands.append(operand1 - operand2)
+                case("/"):
+                    operands.append(operand1 / operand2)
+    
+    #Return result of postfix calculation
+    if(len(operands) == 1):
+        return operands[0]
+    #Final check for if the postfix expression is valid
+    else:
+        print("There was an issue, please check your postfix expression.")
+        return None
 
 
 def parse_args(arglist):
