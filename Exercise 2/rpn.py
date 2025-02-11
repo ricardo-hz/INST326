db80d699832194948241ac277944376beb433474
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-02-10 20:09:29 -0500

    Add docstrings, finalize assignment

--- Exercise 2/rpn.py
+++ Exercise 2/rpn.py
@@ -1,12 +1,18 @@
 from argparse import ArgumentParser
 import sys
-#TODO: Make main method
-#TODO: Read from file
-#TODO: Print steps
 
-# Replace this comment with your implementations of the evaluate() and main()
-# functions.
 def evaluate(postfix_expression):
+    """Evaluates expressions written in postfix notation.
+    
+    Args:
+        postfix_expression (str) : Postfix expression to be evaluted.
+    
+    Returns:
+        float : The result of evaluating the postfix expression.
+    
+    Raises:
+        IndexError : Passed an expression without proper postfix notation.
+    """
     operands = []
     possible_operators = ['+','*','/','-']
     postfix_expression = postfix_expression.strip().split()
@@ -24,8 +30,9 @@ def evaluate(postfix_expression):
                 operand1 = operands.pop()
             #Handle invalid postfix expressions being passed
             except:
-                print("There was an issue, please check your postfix expression.")
-                return None
+                print(f"There was an issue, please check "
+                      f"your postfix expression.")
+                raise
             #Add result of postfix calculation to stack
             match(element):
                 case("+"):
@@ -43,7 +50,21 @@ def evaluate(postfix_expression):
     #Final check for if the postfix expression is valid
     else:
         print("There was an issue, please check your postfix expression.")
-        return None
+        raise
+    
+def main(path):
+    """Reads postfix expressions from a file, prints results to console.
+    
+    Args:
+        path(str) : The path that the postfix expressions should be read from.
+    
+    Side Effects:
+        - Prints the contents of the file to the console, followed by "=" and
+            the return value given by the evaluate function
+    """
+    with open(path, "r", encoding = "utf-8") as f:
+        for line in f:
+            print(f"{line.strip()} = {evaluate(line.strip())}")
 
 
 def parse_args(arglist):
