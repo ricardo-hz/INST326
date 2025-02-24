0d5e1f10ed788c6ad2d40bee1a6d1a34c8841248
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-02-24 10:38:35 -0500

    Update docstrings (submission ver.)

--- Exercise 4/car.py
+++ Exercise 4/car.py
@@ -2,70 +2,71 @@ from math import cos, radians, sin
 
 
 class Car:
-    '''creates a car that can be turned and driven
+    """Creates a car that can be turned and driven.
     
     Attributes:
-        x (float): the starting x coordinate of the car, as a float. Default: 0.
-        y (float): the starting y coordinate of the car, as a float. Default: 0.
-        heading (float): the starting heading, as a float. Default: 0.
-    '''
+        x: A float indicating the starting x position of the car. Default: 0.
+        y: A float indicating the starting y position of the car. Default: 0.
+        heading: A float indicating the starting direction of the car. 
+            Default: 0.
+    """
     
     def __init__(self, x = 0, y = 0, heading = 0):
-        ''' initializes a Car object with x-y coordinates and a heading,
-            all defaulting at 0
+        """ Initializes a Car object.
         
         Attributes:
-            x (float): the starting x coordinate of the car, as a float. Default: 0.
-            y (float): the starting y coordinate of the car, as a float. Default: 0.
-            heading (float): the starting heading, as a float. Default: 0.
+            x: the starting x coordinate of the car, as a float. Default: 0.
+            y: A float indicating starting y coordinate of the car. Default: 0.
+            heading: A float indicating the starting direction of the car in 
+                degrees. A value of 0 indicates north, 90 indicates east,
+                180 indicates south, 270 indicates west. Default: 0.
             
         Side effects:
-            none
-            
-        Returns:
-            none
-        '''
+            Creates attributes for a car object
+        """
         
         self.x = x
         self.y = y
         self.heading = heading
         
     def turn(self, angle):
-        ''' Turns the car by a specified angle
+        """ Turns the car by a specified angle.
 
-        Attributes:
-            angle (float): angle to turn the car by in degrees
-            
+        Args:
+            angle: A float representing the number of degrees by which the car
+                object should be turned.
         Side effects:
-            changes the heading value of the Car object
-
-        Returns:
-            none
-        '''
+            Modifies the car object's heading attribute.
+        """
          
         self.heading += angle
-        
         self.heading = self.heading%360
         
     def drive(self, distance):
-        ''' moves car by a specified distance
+        """ Moves a car object along an (x, y) axis.
         
-        Attributes:
-            distance (float): distance to move the car by
+        Args:
+            distance: A float representing the number of degrees by which the
+                car object should be moved.
             
         Side effects:
-            changes the x and y values of the Car object
+            Modifies the car object's x and y attributes. 
+        """
 
-        Returns:
-            none
-        '''
-        
         self.x += distance*sin(radians(self.heading))
         self.y -= distance*cos(radians(self.heading))
 
 
         
 def sanity_check():
+    """Tests the methods of the Car class.
+    
+    Side effects:
+        Prints text to the console.
+        
+    Returns:
+        A car object.
+    """
     c = Car()
     c.turn(90)
     c.drive(10)
