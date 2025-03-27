d9cbdc2a615fbd85be61d8a7ac33dad6b7f94cf8
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-03-26 21:14:13 -0400

    Add Homework 2

---
+++ Homework 2/program.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from json import load
+from sys import argv
+import relationships
+
+class Person():
+    """Represents a person in a family
+    
+    Attributes:
+        name (str) : The person's name
+        gender (str) : The person's gender, represented as 'f','m', or 'n'
+        parents(list) : The person's parents, if known; may be empty
+        spouse (Person or None): The person's spouse if applicable, None if not
+    """
+    
+    def __init__(self):
+        raise NotImplementedError
