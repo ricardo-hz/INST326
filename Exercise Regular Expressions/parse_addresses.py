d33aceedcaefc15c0df95a58aeff27ba444bcf71
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-03-31 10:25:18 -0400

    Change regular expression, create read_addresses()
    - Original regular expression had an issue parsing 555 Hubbard Ave-Suite 12, Pittsfield MA 01201
    - Known issue: Commas show up in read_addresses() zip return value

--- Exercise Regular Expressions/parse_addresses.py
+++ Exercise Regular Expressions/parse_addresses.py
@@ -7,29 +7,36 @@ import sys
 # and read_addresses() function. Uncomment the __repr__() method below
 # and include it in your Address class.
 class Address():
-    pattern = r"(^\S+) (\D+), (\D+) ([A-Z]{2}) (\d{5})" # Add names for grouping??
+    pattern = r"(?P<street_number>^\S+) (?P<street_name>.+), "\
+        r"(?P<city>\D+) (?P<state>[A-Z]{2}) (?P<zip>\d{5})"
+        
     def __init__(self, address):
         match = re.search(Address.pattern, address)
         if match:
-            #self.address = address
-            self.house_number = match.group(1)
-            self.street = match.group(2)
-            self.city = match.group(3)
-            self.zip = match.group(4)
+            self.address = match.group(0)
+            self.house_number = match.group("street_number")
+            self.street = match.group("street_name")
+            self.city = match.group("city")
+            self.state = match.group("state")
+            self.zip = match.group("zip")
         else:
-            raise ValueError("The string could not be parsed")
+            raise ValueError(f"{address} could not be parsed")
 
-    # def __repr__(self):
-    #     """Return a formal representation of the Address object."""
-    #     return (
-    #         f"address:      {self.address}\n"
-    #         f"house number: {self.house_number}\n"
-    #         f"street:       {self.street}\n"
-    #         f"city:         {self.city}\n"
-    #         f"state:        {self.state}\n"
-    #         f"zip:          {self.zip}"
-    #     )
+    def __repr__(self):
+        """Return a formal representation of the Address object."""
+        return (
+            f"address:      {self.address}\n"
+            f"house number: {self.house_number}\n"
+            f"street:       {self.street}\n"
+            f"city:         {self.city}\n"
+            f"state:        {self.state}\n"
+            f"zip:          {self.zip}"
+        )
 
+def read_addresses(path):
+    with open(path, 'r', encoding="utf-8") as f:
+        address_list = [Address(line) for line in f]
+    return read_addresses
 
 def parse_args(arglist):
     """ Parse command-line arguments.
