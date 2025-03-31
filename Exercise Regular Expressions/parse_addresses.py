3d6329168f46b9163489f8d9bcb0b29f2db93656
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-03-31 10:43:06 -0400

    Add comments, complete exercise

--- Exercise Regular Expressions/parse_addresses.py
+++ Exercise Regular Expressions/parse_addresses.py
@@ -2,15 +2,35 @@ from argparse import ArgumentParser
 import re
 import sys
 
-
-# replace this comment with your implementation of the Address class
-# and read_addresses() function. Uncomment the __repr__() method below
-# and include it in your Address class.
 class Address():
+    """Represents an address
+    
+    Attributes:
+        address (str) : The entire address as a string. The string should not
+            contain leading or trailing spaces.
+        house_number (str) : The house number of the address (e.g., 5173B).
+        street (str): The street name (e.g., 63rd Ave NE).
+        city (str): The city (e.g., Lake Forest Park).
+        state (str): The state (e.g., WA).
+        zip (str): The zip code (e.g., 98155).
+    """
     pattern = r"(?P<street_number>^\S+) (?P<street_name>.+), "\
         r"(?P<city>\D+) (?P<state>[A-Z]{2}) (?P<zip>\d{5})"
         
     def __init__(self, address):
+        """Initializes an address object.
+        
+        Args:
+            address (str) : The entire address as a string with no leading or
+                trailing spaces.
+        
+        Raises:
+            ValueError : raised when an address cannot be parsed by the regular
+                expression.
+    
+        Side effects:
+            Creates an Address object in memory.
+        """
         match = re.search(Address.pattern, address)
         if match:
             self.address = match.group(0)
@@ -33,10 +53,24 @@ class Address():
             f"zip:          {self.zip}"
         )
 
+
 def read_addresses(path):
+    """Reads addresses from a file.
+    
+    Args:
+        path (str) : The path to a file with one address per line.
+    
+    Returns:
+        address_list (list) : A list in which each index is an Address object
+            for the corresponding address in path.
+    
+    Side effects:
+        Creates an address object for each address in path.
+    """
     with open(path, 'r', encoding="utf-8") as f:
         address_list = [Address(line) for line in f]
-    return read_addresses
+    return address_list
+
 
 def parse_args(arglist):
     """ Parse command-line arguments.
